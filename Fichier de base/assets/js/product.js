fb1f8108b4cf3db6c48b87483f4f08d5c3ef6aac
Simon <simont141@gmail.com>
2022-12-31 13:11:05 -0500

    new commit

---
+++ Fichier de base/assets/js/product.js
@@ -0,0 +1,109 @@
+var productList = []; 
+
+$(document).ready(function () {
+    // to get productList from json file and display current product
+    $.ajax({
+        async: true, 
+        type: "GET",
+        url: "./data/products.json",
+        datatype: 'json',
+        success: function(xhr) {
+            //update product list on successful get
+            productList = xhr; 
+
+            //product page must have ?id= suffix to identify the product
+            if (!window.location.href.includes("?id="))
+                window.location.assign("/product.html?id=404"); 
+
+            displayInfoProducts($.urlParam('id')); 
+        }
+    }); 
+});
+
+$(document).ready(function () {
+    //to add product and quantity of products to shopping cart after user submission
+    //create dialog and fade out after 5s
+    var currentUrl = window.location.href; 
+    $("#add-to-cart-btn").click(function () {
+        var id = $.urlParam('id') - 1; 
+        addToLocalStorage(id);
+        openDialog("Le produit a été ajouté au panier.");
+    });
+});
+
+$.urlParam = function(name){
+    // to get id from url
+	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+	return results[1] || 0;
+}
+
+function range(start, end) {
+    //utility function to create an Array with element from start to end
+    //python equivalent: list(range(start, end))
+    return Array.apply(0, Array(end))
+      .map((element, index) => index + start);
+}
+
+function displayInfoProducts(id){
+    // to display data from productList to HTML page
+    //id - 1 because first product in json is 1 while it is 0 in productList
+    index = id - 1; 
+    if (index in range(0, productList.length)){
+        //change innerHTML of image, name, price, description, features
+        $("#product-image").attr("src", "./assets/img/" + productList[index].image);
+        $("#product-name").html(productList[index].name);
+        $("#product-desc").html(productList[index].description);
+        $("#product-price").html('Prix: <strong>'+ productList[index].price +'&thinsp;$</strong>');
+    
+        var featureList = ""; 
+        for (let i = 0; i < productList[index].features.length; i++)
+            featureList += "<li>" + productList[index].features[i] + "</li>"; 
+        $("#product-features").html(featureList);
+    }
+    else{
+        $('article').html('<h1>Page non trouvée!</h1>'); 
+    }
+}
+
+function addToLocalStorage(id) {
+    //"virtual shopping cart", to store the data when user add products to cart
+    //Bcz local storage store data in key:value pair so 
+    //key = item's name
+    //value = JSON object contain: price, quantity, and id 
+    var keyName = "Item #" + (id+1).toString();
+    var currentItem = window.localStorage.getItem(keyName); 
+
+    // create new item if item doesnt exist in local storage
+    if (currentItem == null){
+        var newItem = new Object(); 
+        newItem["id"] = parseInt($.urlParam('id')); 
+        newItem["name"] = productList[id].name;
+        newItem["quantity"] = parseInt($("#product-quantity").val()); 
+        newItem["price"] = parseFloat(productList[id].price); 
+        window.localStorage.setItem(keyName, JSON.stringify(newItem));
+    }
+
+    // update current item if it exists
+    else {
+        currentItem = JSON.parse(currentItem);
+        currentItem.quantity += parseInt($("#product-quantity").val()); 
+        window.localStorage.setItem(keyName, JSON.stringify(currentItem));
+
+
+    }
+    // Update the header item count
+    updateShoppingListCount();
+}
+
+function openDialog(message){
+    //create dialog box
+    $("#dialog").remove();
+    dialogBox = '<dialog open id ="dialog">' + message + '</dialog>';
+    $("article").append(dialogBox); 
+
+    // hide dialog after 5000 ms = 5s and delete to prepare for the next dialog
+    $("#dialog").fadeOut(5000,function() {
+        $(this).modal('hide');
+        $("#dialog").remove();
+    });
+}
---
+++ Fichier de base/assets/js/products3.js
@@ -0,0 +1,139 @@
+var productList = []; 
+var toDisplayList = []; 
+
+$(document).ready(function() {
+    //ajax call to display list of product, update data to productList
+    //and perform a default sort and filter per assignment's request
+    $.ajax({
+        async: true, 
+        type: "GET",
+        url: "./data/products.json",
+        datatype: 'json',
+        success: function(xhr){
+            displayList(xhr); 
+            updateProductList(xhr); 
+            defaultSortAndFilter(); 
+        }
+    }); 
+});
+
+$(document).ready(function() {
+    //to sort item after clicking on sort button
+    $(".sort-btn").click(function(){
+        //assigns the productList and sort after
+        toDisplayList = productList; 
+
+        //match button to correct sort function
+        //bool value in sortProducts mean is descending or not
+        if (this.innerHTML.includes("Nom")){
+            if(this.innerHTML.includes("A-Z"))
+                sortProducts(toDisplayList, "name", false);
+            else {sortProducts(toDisplayList, "name", true);}
+        } else {
+            if(this.innerHTML.includes("bas-haut"))
+                sortProducts(toDisplayList, "price", false);
+            else {sortProducts(toDisplayList, "price", true);}
+        }
+        //display at the end
+        displayList(toDisplayList);
+
+        // de-select old button, select clicked button
+        $(".selected.sort-btn").toggleClass("selected"); 
+        $(this).toggleClass("selected");
+    });
+});
+
+$(document).ready(function() {
+    $(".filter-btn").click(function(){
+        var filterCategory = "";
+
+        //match button to actual category in product list
+        switch(this.innerHTML){
+            case "Appareils photo": filterCategory = "cameras"; break; 
+            case "Consoles": filterCategory = "consoles"; break;
+            case "Écrans": filterCategory = "screens"; break; 
+            case "Ordinateurs": filterCategory = "computers"; break; 
+        }
+
+        // display list
+        if (this.innerHTML == "Tous les produits")
+            toDisplayList = productList; 
+        else
+            toDisplayList = filterProducts(productList, filterCategory); 
+        displayList(toDisplayList);
+
+        // update product count
+        $('#products-count').html(toDisplayList.length + " produit" + (toDisplayList.length ? "s" : "")); 
+
+        // de-select old button, select clicked button
+        $(".selected.filter-btn").toggleClass("selected"); 
+        $(this).toggleClass("selected");
+    })
+    
+}); 
+
+function updateProductList(list){
+    productList = list; 
+}
+
+function displayList(array) {
+    var productCell = ""; 
+    var table = ""; 
+    
+    for(let i = 0; i < array.length; i++){
+        //set class of each to its category
+        productCell += '<div class="product"' + ' class="' + array[i].category + '">';
+
+        //set its redirection link using its ids
+        productCell += '<a href="./product.html?id='+ array[i].id +'" title="En savoir plus...">';
+
+        productCell += '<h2>' + array[i].name + '</h2>';
+        productCell += '<img alt="' + array[i].name + '" src="./assets/img/' + array[i].image + '">';
+        productCell += '<p class="price"><small>Prix</small>' + " " + array[i].price + '&thinsp;$</p>'; 
+
+        productCell += '</a>';
+        productCell += '</div>';
+        
+        table += productCell; 
+        productCell = "";
+    }
+    //replace current content with updated one
+    $("#products-list").html(table); 
+}
+
+function sortProducts(list, criteria, descending){
+    //sort inputed list with 2 criteria and (criteria descending or ascending)
+    switch(criteria){
+        case "name": 
+            list.sort((a,b) => (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : -1);
+            break;
+        case "price":
+            list.sort((a,b) => (a.price > b.price) ? 1 : -1);
+            break; 
+    }
+    if (descending){
+        list.reverse(); 
+    }
+}
+
+function filterProducts(list, category){
+    //return a list of matching category products
+    var filteredList = []; 
+    for(let i = 0; i < list.length; i++){
+        if (list[i].category == category) {
+            filteredList.push(list[i]);
+        }
+    }
+    return filteredList; 
+}
+
+function defaultSortAndFilter(){
+    //default filter all the products and sort with price from high to low
+    var toDisplayList = productList; 
+    sortProducts(toDisplayList, "price", false);
+    displayList(toDisplayList); 
+
+    //change class to gray out current selected button using css
+    $(".filter-btn:contains('Tous les produits')").toggleClass("selected");
+    $(".sort-btn:contains('Prix (bas-haut)')").toggleClass("selected");
+}
